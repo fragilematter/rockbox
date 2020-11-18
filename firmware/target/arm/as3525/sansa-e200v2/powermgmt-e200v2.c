cd207165ebe5e8bbc08dcdd357c7991be9eacea3
fragilematter <barbu.doru@gmail.com>
2020-11-18 11:27:03 +0200

    e200v2: more restrictive battery charge/discharge limits intended to reduce battery wear

--- firmware/target/arm/as3525/sansa-e200v2/powermgmt-e200v2.c
+++ firmware/target/arm/as3525/sansa-e200v2/powermgmt-e200v2.c
@@ -25,25 +25,25 @@
 
 const unsigned short battery_level_dangerous[BATTERY_TYPES_COUNT] =
 {
-    3400
+    3500
 };
 
 const unsigned short battery_level_shutoff[BATTERY_TYPES_COUNT] =
 {
-    3300
+    3400
 };
 
 /* voltages (millivolt) of 0%, 10%, ... 100% when charging disabled */
 const unsigned short percent_to_volt_discharge[BATTERY_TYPES_COUNT][11] =
 {
     /* Sansa Li Ion 750mAH, FIXME copied from e200v1 */
-    { 3300, 3680, 3740, 3760, 3780, 3810, 3870, 3930, 3970, 4070, 4160 },
+    { 3400, 3460, 3520, 3580, 3640, 3700, 3760, 3820, 3880, 3940, 4000 },
 };
 
 /* voltages (millivolt) of 0%, 10%, ... 100% when charging enabled */
 const unsigned short percent_to_volt_charge[11] =
 {
     /* Sansa Li Ion 750mAH FIXME */
-    3300, 3680, 3740, 3760, 3780, 3810, 3870, 3930, 3970, 4070, 4160
+    3400, 3460, 3520, 3580, 3640, 3700, 3760, 3820, 3880, 3940, 4000
 };
 
